a1139413741fe5178c81ec35c83969bac0ee7553
Alexandre Chopin <wikeo65@gmail.com>
2016-11-24 13:46:51 +0100

    clean _list

--- pages/_list.vue
+++ pages/_list.vue
@@ -8,15 +8,9 @@ import ItemList from '~components/ItemList.vue'
 let categories = ['top', 'new', 'show', 'ask', 'job']
 
 export default {
-<<<<<<< HEAD
-  data ({ store, route, error }) {
-    if (categories.indexOf(route.params.category) < 0) {
-      error({ statusCode: 404, messgae: 'Page not found' })
-=======
   fetch ({ store, route, error }) {
     if (categories.indexOf(route.params.category) === -1) {
       return error({ statusCode: 404, message: 'Page not found' })
->>>>>>> origin/master
     }
     return store.dispatch('FETCH_LIST_DATA', { type: route.params.category })
   },
